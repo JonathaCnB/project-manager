2609acda8c9a9ed90a805b5106705da51610bd03
JonathaCnB <jotacarlosftb@gmail.com>
2021-10-21 20:47:41 -0400

    implementando o gerenciament de users via django admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    model = Project
+    list_display = ["id", "name", "start_date", "end_date", "is_active"]
+    search_fields = ["name"]
