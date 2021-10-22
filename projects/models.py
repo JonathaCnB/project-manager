f91e1db7cdd557c410955b61de4a1b9acdc2e72d
JonathaCnB <jotacarlosftb@gmail.com>
2021-10-21 20:39:03 -0400

    criando tabela no banco de dados conforme modelagem de dados

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(
+        max_length=255,
+        verbose_name="Nome do projeto",
+        unique=True,
+    )
+    start_date = models.DateField(verbose_name="Data de início")
+    end_date = models.DateField(verbose_name="Data de término")
+    is_active = models.BooleanField(default=True)
+    value = models.DecimalField(
+        verbose_name="Valor do projeto",
+        max_digits=7,
+        decimal_places=2,
+    )
+    CHOICE_STATUS_RISK = (
+        ("0", "Baixo"),
+        ("1", "Médio"),
+        ("2", "Alto"),
+    )
+    correct_answer = models.CharField(
+        max_length=1,
+        verbose_name="Risco ",
+        choices=CHOICE_STATUS_RISK,
+    )
+    participants = models.ManyToManyField(User, verbose_name='Participantes')
+
+    class Meta:
+        verbose_name = "Projeto"
+        verbose_name_plural = "Projetos"
+        db_table = "projects_project"
+
+    def __str__(self) -> str:
+        return self.name
