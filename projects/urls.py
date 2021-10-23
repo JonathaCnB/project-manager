e04b722e2985dc7aacc96213a8cbf6908e8c8db3
JonathaCnB <jotacarlosftb@gmail.com>
2021-10-23 01:15:55 -0400

    conexão das urls do projeto com sua respectiva view

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from .views import index
+from .views import (
+    calculate_project_risk,
+    create_project,
+    delete_project,
+    index,
+    update_project,
+)
 
-app_name = 'projects'
+app_name = "projects"
 
 urlpatterns = [
-    path('', index, name='index'),
+    path("", index, name="index"),
+    path("create/", create_project, name="create"),
+    path("update/<int:id>/", update_project, name="update"),
+    path("delete/<int:id>/", delete_project, name="delete"),
+    path("calcule-risk/<int:id>/", calculate_project_risk, name="calcule-risk"),
 ]
